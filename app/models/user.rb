88db37e2da441ad5019be8b1a7bedceb15fc1466
kazuya-nagai <k-nagai@eole.co.jp>
2024-07-12 06:36:54 +0000

    micropost setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save   :downcase_email
@@ -74,6 +75,10 @@ class User < ApplicationRecord
   # パスワード再設定のメールを送信する
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+
+  def feed
+    Micropost.where('user_id = ?', id)
   end
 
   private
