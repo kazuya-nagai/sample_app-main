a7d410eaac841e607285a2b00727ae7aa8b67c48
kazuya-nagai <k-nagai@eole.co.jp>
2024-07-22 16:22:42 +0900

    fix server ip address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '54.250.13.123', user: 'ec2-user', roles: %w{app db web}, ssh_options: {
+server '57.180.113.44', user: 'ec2-user', roles: %w{app db web}, ssh_options: {
   keys: %w(~/.ssh/S-accesskey.pem),
   forward_agent: false,
   auth_methods: %w(publickey)
